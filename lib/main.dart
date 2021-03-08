392864f0edb3a516e32ed2f5a9743082cd023830
Felipe Soares <felipe31soares@gmail.com>
2021-03-08 20:46:30 -0300

    Move all content to one singe page

--- lib/main.dart
+++ lib/main.dart
@@ -8,45 +8,7 @@ import 'package:http/http.dart' as http;
 import 'package:toast/toast.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        canvasColor: Color(0xFFe1e1e1e1),
-        primarySwatch: MaterialColor(
-          0xFF313131,
-          <int, Color>{
-            50: Color(0xFF000000),
-            100: Color(0xFF000000),
-            200: Color(0xFF000000),
-            300: Color(0xFF000000),
-            400: Color(0xFF000000),
-            500: Color(0xFF000000),
-            600: Color(0xFF000000),
-            700: Color(0xFF000000),
-            800: Color(0xFF000000),
-            900: Color(0xFF000000),
-          },
-        ),
-      ),
-      home: MyHomePage(title: 'Github Browser'),
-    );
-  }
+  runApp(MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
@@ -176,33 +138,19 @@ class _MyHomePageState extends State<MyHomePage> {
   String username;
 
   void __searchGithub() {
-    Toast.show("Write username and press enter", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+    Toast.show("Write username and press enter", context,
+        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
   }
 
   void _searchGithub(String name) {
-    // setState(() {
-    //   // This call to setState tells the Flutter framework that something has
-    //   // changed in this State, which causes it to rerun the build method below
-    //   // so that the display can reflect the updated values. If we changed
-    //   // _counter without calling setState(), then the build method would not be
-    //   // called again, and so nothing would appear to happen.
-    // });
-    this.username = name;
-
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Github details"),
-          ),
-          body: Center(
-            child: Column(children: [
-              _getGithubString(username),
-            ]),
-          ),
-        );
-      }),
-    );
+    setState(() {
+      //   // This call to setState tells the Flutter framework that something has
+      //   // changed in this State, which causes it to rerun the build method below
+      //   // so that the display can reflect the updated values. If we changed
+      //   // _counter without calling setState(), then the build method would not be
+      //   // called again, and so nothing would appear to happen.
+      this.username = name;
+    });
   }
 
   Widget _getGithubString(String username) {
@@ -214,13 +162,15 @@ class _MyHomePageState extends State<MyHomePage> {
         if (user.hasData) {
           return Row(children: [
             Image.network(user.data.avatar_url, height: 90),
-            Column( children: [
-              Text(user.data.name),
-              Text(user.data.login),
-            ],),
+            Column(
+              children: [
+                Text(user.data.name),
+                Text("@${user.data.login}"),
+              ],
+            ),
           ]);
         } else if (user.hasError) {
-          return Column(children: [Text("${user.error}"), Text("adsfsd")]);
+          return Column(children: [Text("Error. User not found!")]);
         }
         // By default, show a loading spinner.
         return CircularProgressIndicator();
@@ -247,51 +197,86 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Github search"),
-      ),
-      body: Center(
-        child: Container(
-          width: 250,
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Github username'),
-                  onSubmitted: _searchGithub,
-                ),
-              ),
-            ],
+    return MaterialApp(
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          canvasColor: Color(0xFFe1e1e1e1),
+          primarySwatch: MaterialColor(
+            0xFF313131,
+            <int, Color>{
+              50: Color(0xFF000000),
+              100: Color(0xFF000000),
+              200: Color(0xFF000000),
+              300: Color(0xFF000000),
+              400: Color(0xFF000000),
+              500: Color(0xFF000000),
+              600: Color(0xFF000000),
+              700: Color(0xFF000000),
+              800: Color(0xFF000000),
+              900: Color(0xFF000000),
+            },
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: __searchGithub,
-        tooltip: 'Increment',
-        child: Icon(Icons.arrow_forward),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text("Github search"),
+          ),
+          body: Center(
+            child: Container(
+              width: 250,
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Github username'),
+                      onSubmitted: _searchGithub,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(children: [
+                      _getGithubString(username),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: __searchGithub,
+            tooltip: 'Increment',
+            child: Icon(Icons.arrow_forward),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 }
