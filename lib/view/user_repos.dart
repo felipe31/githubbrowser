06947df6aab5d7339c465a76591f9cd73e65d784
Felipe Soares <felipe31soares@gmail.com>
2021-03-28 15:21:35 -0300

    Repository list layout renewed

--- lib/view/user_repos.dart
+++ lib/view/user_repos.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:github_browser/controller/github_api.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:github_browser/model/github_repository.dart';
 import 'package:github_browser/model/github_repository_list.dart';
 import 'package:github_browser/model/github_user.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UsersRepos extends StatelessWidget {
   final GithubUser user;
@@ -48,32 +50,32 @@ class UsersRepos extends StatelessWidget {
           return FutureBuilder<GithubReposList>(
               future: _response,
               builder: (context, repos) {
-                if (repos.hasData) {
-                    // new List<GithubRepository>.from(dynamic.data);
-                     // TODO go back to the top of the list when textfield is edited
-                    return ListView.separated(
-                        separatorBuilder: (BuildContext context, int index) =>
-                            Divider(),
-                        itemCount: repos.data.githubRepoList.length,
-                        itemBuilder: (context, index) {
-                          print("index $index");
-                          // print("total ${repos.data}");
-                          return buildRepoCard(repos.data.githubRepoList.elementAt(index));
-                        });
+                if (repos.hasData && repos.data.githubRepoList.length > 0) {
+                  // new List<GithubRepository>.from(dynamic.data);
+                  // TODO go back to the top of the list when textfield is edited
+                  return ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) =>
+                          Divider(),
+                      itemCount: repos.data.githubRepoList.length,
+                      itemBuilder: (context, index) {
+                        print("index $index");
+                        return buildRepoCard(
+                            repos.data.githubRepoList.elementAt(index));
+                      });
                 } else if (repos.hasError) {
                   return Column(children: [Text("Error. Try again!")]);
                 }
                 return ListView.separated(
                     separatorBuilder: (BuildContext context, int index) =>
                         Divider(),
-                    itemCount: 100,
+                    itemCount: 15,
                     itemBuilder: (context, index) {
                       return Center(
                           child: Container(
                               padding: EdgeInsets.symmetric(
                                   vertical: 10, horizontal: 10),
-                              width: 90,
-                              height: 90,
+                              width: 40,
+                              height: 40,
                               child: CircularProgressIndicator()));
                     });
               });
@@ -81,7 +83,59 @@ class UsersRepos extends StatelessWidget {
       ]),
     );
   }
+
   Widget buildRepoCard(GithubRepository repo) {
-    return Text(repo.name);
+    return GestureDetector(
+        onTap: () => launch(repo.htmlUrl),
+        child: Container(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            height: 50,
+            child: Row(children: [
+              Expanded(
+                child: Column(children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      repo.name,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 6),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: () {
+                        return repo.description == null
+                            ? Text("")
+                            : Text(
+                                repo.description,
+                                textAlign: TextAlign.left,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              );
+                      }(),
+                    ),
+                  )
+                ]),
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 10, left: 20),
+                // TODO add list of languages
+                child: () {
+                  return repo.language == null
+                      ? Text("")
+                      : Text(
+                          repo.language,
+                          style: TextStyle(color: Colors.black45),
+                        );
+                }(),
+              ),
+            ])));
   }
 }
