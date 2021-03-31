529e515d37c0dbf84e4a045f023f115f67282550
Felipe Soares <felipe31soares@gmail.com>
2021-03-30 23:26:18 -0300

    Add SilverAppBar

--- lib/view/user_repos.dart
+++ lib/view/user_repos.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:github_browser/model/github_repository.dart';
 import 'package:github_browser/model/github_repository_list.dart';
 import 'package:github_browser/model/github_user.dart';
@@ -13,74 +13,82 @@ class UsersRepos extends StatelessWidget {
 
   UsersRepos(this.user) {
     this._response = GithubReposList.fetchRepos(user.reposUrl);
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('@${user.login}'),
-      ),
-      body: Column(children: [
-        // TODO replace the action bar with a picture-login rectangle (a la linkedin)
-        Container(
-            decoration: BoxDecoration(
-                color: Color(0xFFFFFFFFFF),
-                border: Border.all(color: Color(0xff838383), width: 1),
-                borderRadius: BorderRadius.circular(90)),
-            padding: EdgeInsets.all(3),
-            margin: EdgeInsets.only(top: 10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(90.0),
-              child: Image.network(
-                user.avatarUrl,
-                height: 100,
-                width: 100,
-              ),
-            )),
-        Center(
-          child: ElevatedButton(
-            child: Text(user.login),
-            onPressed: () {
-              // Navigate to second route when tapped.
-            },
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            snap: false,
+            floating: false,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+
+                title: Text(user.login),
+                background: Container(
+                    decoration: BoxDecoration(
+                    // color: const Color(0x00e1e1e1),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                          image: NetworkImage(
+                          user.avatarUrl,
+                      )
+                ),
+))
+            ),
           ),
-        ),
-        Expanded(child: () {
-          return FutureBuilder<GithubReposList>(
-              future: _response,
-              builder: (context, repos) {
-                if (repos.hasData && repos.data.githubRepoList.length > 0) {
-                  // new List<GithubRepository>.from(dynamic.data);
-                  // TODO go back to the top of the list when textfield is edited
-                  return ListView.separated(
-                      separatorBuilder: (BuildContext context, int index) =>
-                          Divider(),
-                      itemCount: repos.data.githubRepoList.length,
-                      itemBuilder: (context, index) {
-                        print("index $index");
-                        return buildRepoCard(
-                            repos.data.githubRepoList.elementAt(index));
+          SliverFillRemaining(
+            child: Column(
+              children: [
+                Expanded(child: () {
+                  return FutureBuilder<GithubReposList>(
+                      future: _response,
+                      builder: (context, repos) {
+                        if (repos.hasData &&
+                            repos.data.githubRepoList.length > 0) {
+                          // new List<GithubRepository>.from(dynamic.data);
+                          // TODO go back to the top of the list when textfield is edited
+                          return ListView.separated(
+                              physics: const NeverScrollableScrollPhysics(),
+                              separatorBuilder:
+                                  (BuildContext context, int index) =>
+                                  Divider(),
+                              itemCount: repos.data.githubRepoList.length,
+                              itemBuilder: (context, index) {
+                                print("index $index");
+                                return buildRepoCard(
+                                    repos.data.githubRepoList.elementAt(index));
+                              });
+                        } else if (repos.hasError) {
+                          return Column(children: [Text("Error. Try again!")]);
+                        }
+                        return ListView.separated(
+                            separatorBuilder: (BuildContext context,
+                                int index) =>
+                                Divider(),
+                            itemCount: 15,
+                            itemBuilder: (context, index) {
+                              return Center(
+                                  child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 10),
+                                      width: 40,
+                                      height: 40,
+                                      child: CircularProgressIndicator()));
+                            });
                       });
-                } else if (repos.hasError) {
-                  return Column(children: [Text("Error. Try again!")]);
-                }
-                return ListView.separated(
-                    separatorBuilder: (BuildContext context, int index) =>
-                        Divider(),
-                    itemCount: 15,
-                    itemBuilder: (context, index) {
-                      return Center(
-                          child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 10),
-                              width: 40,
-                              height: 40,
-                              child: CircularProgressIndicator()));
-                    });
-              });
-        }()),
-      ]),
+                }()),
+              ],
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 
@@ -112,13 +120,13 @@ class UsersRepos extends StatelessWidget {
                         return repo.description == null
                             ? Text("")
                             : Text(
-                                repo.description,
-                                textAlign: TextAlign.left,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                ),
-                              );
+                          repo.description,
+                          textAlign: TextAlign.left,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        );
                       }(),
                     ),
                   )
@@ -131,9 +139,9 @@ class UsersRepos extends StatelessWidget {
                   return repo.language == null
                       ? Text("")
                       : Text(
-                          repo.language,
-                          style: TextStyle(color: Colors.black45),
-                        );
+                    repo.language,
+                    style: TextStyle(color: Colors.black45),
+                  );
                 }(),
               ),
             ])));
